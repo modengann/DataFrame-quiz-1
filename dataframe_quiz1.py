a93c0e6dcd8eda5698e098b6fef72cbafab020e6
CaseyGann <cmoden@gannacademy.org>
2023-10-31 08:42:55 -0400

    init

---
+++ dataframe_quiz1.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+   
+def get_department(df):
+    pass
+    
+def get_second_record(df):
+    pass
+    
+def get_info_grace(df):
+    pass
+
+def get_experience_location(df):
+    pass
+
+def get_experience_salary_top_three(df):
+    pass
+    
+def get_department_elena_aiden(df):   
+    pass
+
+def main():
+    df = pd.read_csv("data.csv")
+    print(get_department(df))
+
+if __name__ == "__main__":
+        main()
