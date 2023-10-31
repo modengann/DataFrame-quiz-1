819adabb3230fac49584d4e067d2c23c6d4768d5
CaseyGann <cmoden@gannacademy.org>
2023-10-31 08:49:39 -0400

    fixed

--- dataframe_quiz1.py
+++ dataframe_quiz1.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-
-   
 def get_department(df):
     pass
     
@@ -21,8 +19,15 @@ def get_department_elena_aiden(df):
     pass
 
 def main():
-    df = pd.read_csv("data.csv")
-    print(get_department(df))
+    df = pd.read_csv("data.csv", index_col= 0)
+
+    print("Department Column:\n", get_department(df))
+    print("\nSecond Record:\n", get_second_record(df))
+    print("\nGrace's Info:\n", get_info_grace(df))
+    print("\nExperience and Location Columns:\n", get_experience_location(df))
+    print("\nExperience and Salary of Top Three:\n", get_experience_salary_top_three(df))
+    print("\nDepartment of Elena and Aiden:\n", get_department_elena_aiden(df))
+
 
 if __name__ == "__main__":
         main()
